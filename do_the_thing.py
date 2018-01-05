1e7348b19ee533dfa9cf8cf2a1de211841ac8887
Laura Hasemeyer <lbhasemeyer@gmail.com>
2018-01-05 12:47:20 -0700

    Moving all the things to one_hots.

--- do_the_thing.py
+++ do_the_thing.py
@@ -5,6 +5,7 @@ import csv
 import os
 from nltk.corpus import stopwords
 from keras.preprocessing.text import Tokenizer
+import keras.preprocessing.text as kpt
 from keras.preprocessing.sequence import pad_sequences
 from keras.utils import to_categorical
 from keras.layers import Dense, Input, GlobalMaxPooling1D
@@ -19,25 +20,27 @@ PRETRAIN_LABEL_COUNT_MAX = 320
 
 all_features = []
 pretrain_features = []
-# Dictionary for our label encoding
 labels_index = {}
-# Array of labels in numeric encoding
 all_labels = []
 pretrain_labels = []
 labels_count = {}
 
-all_data = pandas.read_csv('./training_data.csv', usecols=['Country', 'FormalEducation', "PronounceGIF"])
+all_data = pandas.read_csv('./training_data.csv', usecols=['Country', 'FormalEducation', 'PronounceGIF'])
 for _, sample in all_data.iterrows():
     if sample["PronounceGIF"] not in labels_index:
         labels_index[sample["PronounceGIF"]] = len(labels_index)
         labels_count[sample["PronounceGIF"]] = 0
-    all_features.append([sample['Country'], sample['FormalEducation']])
+    # all_features.append([sample['Country'], sample['FormalEducation']])
+    wordsForFeature = sample['Country'] + sample['FormalEducation']
+    all_features.append(kpt.one_hot(wordsForFeature, 300, lower=True))
     all_labels.append(labels_index[sample["PronounceGIF"]])
     labels_count[sample["PronounceGIF"]] += 1
-  # if we don't have too much pre-training data for the current label, add it to pretraining data
     if labels_count[sample['PronounceGIF']] < PRETRAIN_LABEL_COUNT_MAX:
-        pretrain_features.append([sample['Country'], sample['FormalEducation']])
+        wordsForFeature = sample['Country'] + sample['FormalEducation']
+        pretrain_features.append(kpt.one_hot(wordsForFeature, 300, lower=True))
         pretrain_labels.append(labels_index[sample["PronounceGIF"]])
+all_labels = to_categorical(np.asarray(all_labels))
+pretrain_labels = to_categorical(np.asarray(pretrain_labels))
 
 # invert dictionary to look up labels later
 labels_lookup = {v: k for k, v in labels_index.items()}
@@ -47,5 +50,7 @@ test_data = pandas.read_csv('./test_data.csv', usecols=['Id', 'Country', 'Formal
 test_features = []
 test_ids = []
 for _, sample in test_data.iterrows():
-    test_features.append([sample['Country'], sample['FormalEducation']])
+    # test_features.append([sample['Country'], sample['FormalEducation']])
+    wordsForFeature = sample['Country'] + sample['FormalEducation']
+    all_features.append(kpt.one_hot(wordsForFeature, 300, lower=True))
     test_ids.append(sample['Id'])
